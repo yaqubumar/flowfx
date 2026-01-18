d546815a5a98167d9a2ad8bcc7aa833ad1808d41
yaqubumar <u.yaqub@hotmail.com>
2026-01-18 15:20:07 +0000

    `Added initial JavaScript code for currency converter`

---
+++ asset/js/script.js
@@ -0,0 +1,8 @@
+/* select and store DOM elements */
+const amountInput = document.getElementById('amount');
+const fromSelect = document.getElementById('fromCurrency');
+const toSelect = document.getElementById('toCurrency');
+const form = document.getElementById('converterForm');
+const resultDiv = document.getElementById('result');
+const themeToggle = document.getElementById('themeToggle');
+const swapBtn = document.getElementById('swapBtn');
